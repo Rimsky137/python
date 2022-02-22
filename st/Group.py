a784febe325d5da215fe3ecb7e0d290d8e38be38
Rimsky137 <100221589+Rimsky137@users.noreply.github.com>
2022-02-22 18:58:46 +0200

    Add files via upload

---
+++ st/Group.py
@@ -0,0 +1,33 @@
+class TooManyStudentsException(Exception):
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+    def get_exception_message(self):
+        return self.message
+class Group:
+    def __init__(self):
+        self.student_list = []
+
+    def add_student(self, student):
+        if len(self.student_list) < 10:
+            self.student_list.append(student)
+        else:
+            try:
+                raise TooManyStudentsException("Too many students!")
+            except TooManyStudentsException as err:
+                print(err.get_exception_message())
+
+    def remove_student(self, student):
+        self.student_list.remove(student)
+
+    def search(self, name):
+        for student in self.student_list:
+            if student.name == name:
+                return student.__str__()
+
+    def __str__(self):
+        str = ""
+        for student in self.student_list:
+            str += student.__str__()
+        return str      
