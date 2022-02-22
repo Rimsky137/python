867c5c290ac46c2834ba37b346cc711bde180c07
Rimsky137 <100221589+Rimsky137@users.noreply.github.com>
2022-02-22 18:17:09 +0200

    Create Product.py

---
+++ python4/Product.py
@@ -0,0 +1,10 @@
+class Product:
+
+    def __init__(self, name, price, description, length, width,):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.length = length
+        self.width = width
+    def __str__(self):
+        return "name = {}, price = {}, description = {}, length = {}, width = {}".format(self.name, self.price, self.description, self.length, self.width)
